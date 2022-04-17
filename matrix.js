194c79371248694887ac97358a179d4ce6d10a0a
Hannes Flaschentraeger <hannesflaschentraeger@gmail.com>
2022-04-17 19:38:18 +0200

    new try

--- matrix.js
+++ matrix.js
@@ -1,21 +1,12 @@
 const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
 canvas.width = 200;
 canvas.height = 400;
+const ctx = canvas.getContext("2d");
 ctx.scale(20, 20);
-
 const COLS = 10;
 const ROWS = 20;
-const columns = [];
 
 const letterSet = "abcdefghijklmnopqrstuvwxyz".split("");
-
-function drawCanvas(x, y, w, h) {
-  ctx.fillStyle = "black";
-  ctx.fillRect(x, y, w, h);
-}
-drawCanvas(0, 0, canvas.width, canvas.height);
-
 
 function random(max, min = 0) {
   min = Math.ceil(min);
@@ -23,59 +14,67 @@ function random(max, min = 0) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+
 class Column {
-  constructor(x, delay) {
+  constructor(x, letters) {
     this.x = x;
-    this.delay = delay;
-    this.chars = [];
+    this.letters = letters;
   }
 }
 
-class Char {
-  constructor(type, x, y) {
-    this.type = type;
+class Letter {
+  constructor(char, x, y) {
+    this.char = char;
     this.x = x;
     this.y = y;
   }
 }
 
-// create columns
-for (let i = 0; i < COLS; i++) {
-  columns.push(new Column(i, random(3000)));
-  //columns[i].chars.push(new Char(letterSet[random(26)], i, 1));
+function drawCanvas() {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
+drawCanvas();
 
-function update() {
+
+
+const columns = [];
+
+(function createColumns() {
+  for (let x = 0; x < COLS; x++) {
+    columns.push(
+      new Column(
+        x,
+        Array(random(5, 15)).fill()
+          .map((_, i) => new Letter(letterSet[random(25)], x, -i))
+      )
+    )
+  }
+})();
+
+
+
+setInterval(() => {
+  move();
+  drawCanvas()
   draw();
-  requestAnimationFrame(update);
-}
-update();
+}, 1000)
 
-setInterval( () => {
-  columns.forEach((column, x, arr) => {
-    setTimeout(() => {
-  
-        column.chars.unshift(
-          new Char(
-            letterSet[random(letterSet.length - 1)], // type
-            x,                    // x pos
-            column.chars.length   // y pos
-          )
-        );
-    }, column.delay)
+function move() {
+  columns.forEach(column => {
+    column.letters.forEach(letter => {
+      letter.y++;
+    });
   })
-}, 100)
-
+}
 
 function draw() {
-  columns.forEach((col, i) => {
-    if (col.chars.length === 0) return; // skip empty first row until it is created
-    ctx.fillStyle = "lime";
-    ctx.font = "1px Arial";
-    ctx.fillText(
-      col.chars[0].type,
-      col.chars[0].x,
-      col.chars[0].y,
-    );
+  ctx.fillStyle = "rgba(0, 255, 0, 1)";
+  ctx.font = "1px Arial";
+
+  columns.forEach((column, i) => {
+    column.letters.forEach((letter, j) => {
+      ctx.fillText(letter.char, letter.x, letter.y + 1);
+    })
   })
 }
