d81b9c9277338cd10c3b00332d51ff3dca13120a
Hannes Flaschentraeger <hannesflaschentraeger@gmail.com>
2022-04-17 20:29:43 +0200

    mehh

--- matrix.js
+++ matrix.js
@@ -1,12 +1,13 @@
 const canvas = document.querySelector("canvas");
-canvas.width = 200;
-canvas.height = 400;
+canvas.width = 2000;
+canvas.height = 4000;
 const ctx = canvas.getContext("2d");
 ctx.scale(20, 20);
-const COLS = 10;
-const ROWS = 20;
+const COLS = 100;
+const ROWS = 200;
 
-const letterSet = "abcdefghijklmnopqrstuvwxyz".split("");
+//const letterSet = "abcdefghijklmnopqrstuvwxyz".split("");
+const letterSet = "0123456789".split("");
 document.querySelector("button").addEventListener("click", createColumns);
 
 function random(max, min = 0) {
@@ -43,13 +44,13 @@ let columns = [];
 
 function createColumns() {
   for (let x = 0; x < COLS; x++) {
-    let offset = random(10);
+    let offset = random(1000);
 
     columns.push(
       new Column(
         x,
-        Array(random(5, 15)).fill()
-          .map((_, i) => new Letter(letterSet[random(25)], x, -i - offset)),
+        Array(random(15, 55)).fill()
+          .map((_, i) => new Letter(letterSet[random(letterSet.length - 1)], x, -i - offset)),
         //random(2000, 2000)
       )
     )
@@ -57,6 +58,10 @@ function createColumns() {
   }
 }
 createColumns();
+
+setInterval(() => {
+  createColumns();
+}, random(5000, 1000))
 
 
 
@@ -79,7 +84,7 @@ function draw() {
   
   columns.forEach((column, i) => {
     column.letters.forEach((letter, j) => {
-      ctx.fillStyle = `rgba(0, 255, 0, ${1 - 0.05 * letter.y})`;
+      ctx.fillStyle = `rgba(0, 255, 0, ${1 - 0.025 * letter.y})`;
       ctx.fillText(letter.char, letter.x, letter.y + 1);
     })
   })
