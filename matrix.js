9241e73d7dee9a616fbe94493468db4eb605a8b9
Hannes Flaschentraeger <hannesflaschentraeger@gmail.com>
2022-04-18 09:37:25 +0200

    modulized project

--- matrix.js
+++ matrix.js
@@ -1,129 +1,19 @@
-const canvas = document.querySelector("canvas");
-canvas.width = innerWidth;
-canvas.height = innerHeight;
-const ctx = canvas.getContext("2d");
-const CELL_SIZE = 20;
-ctx.scale(CELL_SIZE, CELL_SIZE);
-const COLS = canvas.width / CELL_SIZE;
-const ROWS = canvas.height / CELL_SIZE;
-const MIN_LENGTH = (canvas.height / CELL_SIZE) / 5;
-const MAX_LENGTH = canvas.height / CELL_SIZE;
-const OPAC_FACTOR = 0.002;
-const FONT = "1px Arial";
-let columns = [];
+import { ctx } from "./lib/canvas.js";
+import { draw, move } from "./lib/chars.js";
+import { createStartColumns, insertNewColumns } from "./lib/columns.js";
 
-//const CHAR_SET = "abcdefghijklmnopqrstuvwxyz".split("");
-const CHAR_SET = "0123456789".split("");
+onload = () => {
+  ctx.scale(20, 20);
+  createStartColumns();
 
-function random(max, min = 0) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  setInterval(() => {
+    move();
+    draw();
+  }, 100)
 }
 
-
-class Column {
-  constructor(x, chars) {
-    this.x = x;
-    this.chars = chars;
-  }
-}
-
-class Char {
-  constructor(char, x, y) {
-    this.char = char;
-    this.x = x;
-    this.y = y;
-    this.opac = 1;
-  }
-}
-
-function drawCanvas() {
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
-drawCanvas();
-
-
-(function createStartColumns() {
-  for (let x = 0; x < COLS; x++) {
-
-    // add this to y-pos to get random entry times
-    let offset = random(100);
-
-    columns.push(
-      new Column(
-        x,
-        Array(random(MIN_LENGTH, MAX_LENGTH)).fill()
-          .map((_, i) =>
-            new Char(
-              CHAR_SET[random(CHAR_SET.length - 1)],
-              x,
-              -i - offset,
-            )
-          ),
-      )
-    )
-  }
-})();
-
-(function check() {
-  requestAnimationFrame(check);
+(function update() {
+  requestAnimationFrame(update);
   draw();
   insertNewColumns();
 })();
-
-function insertNewColumns() {
-  columns.forEach((column, x) => {
-    if (
-      column.chars.every(char => char.y > canvas.height) ||
-      column.chars.every(char => char.opac <= 0)
-    ) {
-      columns.splice(
-        x,
-        1,
-        new Column(
-          x,
-          Array(random(MIN_LENGTH, MAX_LENGTH)).fill()
-          .map((_, i) =>
-            new Char(
-              CHAR_SET[random(CHAR_SET.length - 1)],
-              x,
-              -i
-            )
-          ),
-        )
-      )
-    }
-  })
-}
-
-setInterval(() => {
-  move();
-  drawCanvas()
-  draw();
-}, 100)
-
-function move() {
-  columns.forEach(column => {
-    column.chars.forEach(char => {
-      char.y++;
-    })
-  })
-}
-
-function draw() {
-  ctx.font = FONT;
-
-  columns.forEach((column, i) => {
-    column.chars.forEach((char, j) => {
-      if (char.y > 0) {
-        ctx.fillStyle = `rgba(0, 255, 0, ${char.opac})`;
-        ctx.fillText(char.char, char.x, char.y + 1);
-      }
-      if (char.y > canvas.height / random(25, 22)) {
-        char.opac -= Math.random() * OPAC_FACTOR * char.y;
-      }
-    })
-  })
-}
