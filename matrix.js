812c083997060f2f26c72a9f579cbfdde2b57ad0
Hannes Flaschentraeger <hannesflaschentraeger@gmail.com>
2022-04-18 15:03:44 +0200

    refactoring

--- matrix.js
+++ matrix.js
@@ -1,7 +1,6 @@
-import random from "./helpers.js";
 import { ctx } from "./lib/canvas.js";
-import Char, { CHAR_SET } from "./lib/chars.js";
-import Column, { columns } from "./lib/columns.js";
+import Char from "./lib/chars.js";
+import Column from "./lib/columns.js";
 
 onload = () => {
   ctx.scale(20, 20);
