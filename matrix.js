6e5dfefcbbda62e3835a8cb62bb8de6a57f5f0ba
Hannes Flaschentraeger <hannesflaschentraeger@gmail.com>
2022-04-18 09:46:03 +0200

    more refactoring

--- matrix.js
+++ matrix.js
@@ -1,19 +1,19 @@
 import { ctx } from "./lib/canvas.js";
-import { draw, move } from "./lib/chars.js";
-import { createStartColumns, insertNewColumns } from "./lib/columns.js";
+import Char from "./lib/chars.js";
+import Column from "./lib/columns.js";
 
 onload = () => {
   ctx.scale(20, 20);
-  createStartColumns();
+  Column.createStartColumns();
 
   setInterval(() => {
-    move();
-    draw();
+    Char.move();
+    Char.draw();
   }, 100)
 }
 
 (function update() {
   requestAnimationFrame(update);
-  draw();
-  insertNewColumns();
+  Char.draw();
+  Column.insertNewColumns();
 })();
